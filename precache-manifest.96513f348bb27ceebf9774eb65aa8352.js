34dc33ffd920e14d59f045b9a29fbf9b84ddc4f4
hanheesong <gmlthd0908@gmail.com>
2018-12-07 17:05:47 +0900

    Updates

---
+++ precache-manifest.96513f348bb27ceebf9774eb65aa8352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aff0d97d0e486b54c2e",
+    "url": "/todolist/static/css/main.75d80584.chunk.css"
+  },
+  {
+    "revision": "9aff0d97d0e486b54c2e",
+    "url": "/todolist/static/js/main.9aff0d97.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/todolist/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c2887326afab2899a4e2e2ff2bb332e2",
+    "url": "/todolist/index.html"
+  }
+];
